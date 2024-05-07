6506dfa7fff11512348d85588e5d3b272274062e
Vilas Chitrakaran <cvilas@gmail.com>
2024-05-07 21:34:32 +0100

    base code

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -1,17 +1,29 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdexcept>
+#include <cstdint>
 
 class TriangleApp {
 public:
     TriangleApp();
     ~TriangleApp();
+    void run();
 private:
+    void initWindow();
+    void initVulkan();
+    void cleanup();
     void mainLoop();
+
+    static constexpr std::uint32_t WIN_WIDTH = 800;
+    static constexpr std::uint32_t WIN_HEIGHT = 600;
+    GLFWwindow* window_{nullptr};
 };
 
 int main() {
     try{
         auto app = TriangleApp();
+	app.run();
         return EXIT_SUCCESS;
     } catch(const std::exception& ex) {
         std::cout << ex.what();
@@ -19,5 +31,35 @@ int main() {
     }
 }
 
-TriangleApp::TriangleApp() {}
+TriangleApp::TriangleApp() {
+
+}
+
 TriangleApp::~TriangleApp() {}
+
+void TriangleApp::run() {
+	initWindow();
+	initVulkan();
+	mainLoop();
+	cleanup();
+}
+
+void TriangleApp::initWindow() {
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window_ = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
+void TriangleApp::initVulkan() {}
+
+void TriangleApp::cleanup() {
+	glfwDestroyWindow(window_);
+	glfwTerminate();
+}
+
+void TriangleApp::mainLoop() {
+	while(not glfwWindowShouldClose(window_)) {
+		glfwPollEvents();
+	}
+}
